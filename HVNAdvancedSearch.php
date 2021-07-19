6a80ebc2d13ba540f10f755913b633e9d5d5ee69
Meir <nicolevianth@gmail.com>
2021-07-19 14:47:11 +0700

    Update HVNAdvancedSearch.php

--- HVNAdvancedSearch.php
+++ HVNAdvancedSearch.php
@@ -9,6 +9,6 @@ curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($c, CURLOPT_HTTPHEADER, array(
     'Cookie: user_id=108808; user_pass=; user_name=Guest User;',
 ));
-echo str_replace($domain . "/forum/search-plus.php", "/HVNAdvancedSearch.php", str_replace('href="/', 'href="' . $domain, curl_exec($cURLConnection)));
+echo str_replace($domain . "/forum/search-plus.php", "./HVNAdvancedSearch.php", str_replace('href="/', 'href="' . $domain, curl_exec($cURLConnection)));
 curl_close($cURLConnection);
 ?>
